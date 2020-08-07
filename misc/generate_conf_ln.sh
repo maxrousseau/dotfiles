09d72f0973bbbb76e1d27755e5e202b08be82460
Maxime Rousseau <maximerousseau08@gmail.com>
2020-08-06 21:17:03 -0400

    added rofi config

--- misc/generate_conf_ln.sh
+++ misc/generate_conf_ln.sh
@@ -5,3 +5,4 @@ ln -sf /home/max/src/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -sf /home/max/src/dotfiles/emacs/settings.org ~/.emacs.d/settings.org
 ln -sf /home/max/src/dotfiles/xterm/.Xresources ~/.Xresources
 ln -sf /home/max/src/dotfiles/misc/.Xmodmap ~/.Xmodmap
+ln -sf /home/max/src/dotfiles/openbox/* ~/.config/openbox/
