c4bb98277bbec6cfe91b465c7d10565d2478cb03
maxrousseau <maximerousseau08@gmail.com>
2020-08-09 10:44:37 -0400

    shell script skeleton

--- misc/generate_conf_ln.sh
+++ misc/generate_conf_ln.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+# small cli to help manage linux system
 
+# soft linking all config files
 ln -sf /home/max/src/dotfiles/bash/.bashrc ~/.bashrc
 ln -sf /home/max/src/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -sf /home/max/src/dotfiles/emacs/settings.org ~/.emacs.d/settings.org
@@ -7,3 +9,13 @@ ln -sf /home/max/src/dotfiles/xterm/.Xresources ~/.Xresources
 ln -sf /home/max/src/dotfiles/misc/.Xmodmap ~/.Xmodmap
 ln -sf /home/max/src/dotfiles/openbox/* ~/.config/openbox/
 ln -sf /home/max/src/dotfiles/rofi/* ~/.config/rofi
+
+# terminal, tmux and shell
+# emacs
+# rofi
+
+# fonts
+# packages
+# system health checks
+
+# MOTD and startup (maybe do this in elisp?)
